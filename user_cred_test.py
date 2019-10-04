5d5708f24bed49dc7fb85a0e8203d9f1c3e39acc
RKNyra <rk.nyra@gmail.com>
2019-10-05 02:44:08 +0300

    run the first test to see if the user objects are being instantiated correctly

--- user_cred_test.py
+++ user_cred_test.py
@@ -6,6 +6,23 @@ class TestUser(unittest.TestCase):
   Test class that defines the test cases for the User class behavior.
   
   Args:
-      unittest.Testcase: TestCase class that helps in creating cases
+      unittest.Testcase: TestCase class that helps in creating cases.
   '''
   
+  def setUp(self):
+    '''
+    Method to create a new instance of the User class before each test case to be declared.
+    '''
+    
+    self.new_user = User('Stl','pswd1234')
+    
+  def test__init__(self):
+    '''
+    Test if the creation of new User instances is properly done.
+    '''
+    self.assertEqual(self.new_user.username, 'Stl')
+    self.assertEqual(self.new_user.password,'pswd1234')
+    
+    
+if __name__ == '__main__':
+  unittest.main()
