3d0e1ed7f2cc641851280d13da07720419ac2195
RKNyra <rk.nyra@gmail.com>
2019-10-05 02:28:39 +0300

    create a user_cred_test.py file where all the tests for the User and Credentials classes will be made

---
+++ user_cred_test.py
@@ -0,0 +1,11 @@
+import unittest # Import the unittest module
+from user import User # Imiport the User class
+
+class TestUser(unittest.TestCase):
+  '''
+  Test class that defines the test cases for the User class behavior.
+  
+  Args:
+      unittest.Testcase: TestCase class that helps in creating cases
+  '''
+  
