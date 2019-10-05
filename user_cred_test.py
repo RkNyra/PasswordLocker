bc63768f061e7bbc9fcb80f84291c9250b2c7b94
RKNyra <rk.nyra@gmail.com>
2019-10-05 03:09:28 +0300

    run a test in the test.py file to check whether new users are saved into the users list and write the method in the User class module to save new users

--- user_cred_test.py
+++ user_cred_test.py
@@ -16,12 +16,22 @@ class TestUser(unittest.TestCase):
     
     self.new_user = User('Stl','pswd1234')
     
+    # ======== FIRST TEST - CORRECT INSTANTIATION ====
   def test__init__(self):
     '''
     Test if the creation of new User instances is properly done.
     '''
     self.assertEqual(self.new_user.username, 'Stl')
     self.assertEqual(self.new_user.password,'pswd1234')
+    
+    
+    # ======= SECOND TEST - SAVING NEW USER =======
+  def test_save_new_user(self):
+    '''
+    Test if the user object is saved into the users list
+    '''
+    self.new_user.save_new_user() # saving the new user
+    self.assertEqual(len(User.users),1)
     
     
 if __name__ == '__main__':
