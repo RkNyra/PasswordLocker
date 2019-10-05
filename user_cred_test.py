8b438d982a32d467c9bcb3c5d355a38818a61b4e
RKNyra <rk.nyra@gmail.com>
2019-10-05 03:56:04 +0300

    run a test in the to check if the Credentials objects are being instantiated correctly

--- user_cred_test.py
+++ user_cred_test.py
@@ -1,22 +1,23 @@
 import unittest # Import the unittest module
-from user import User # Imiport the User class
+from user import User # Import the User class
+from credentials import Credentials # Import the Credentials class
 
 class TestUser(unittest.TestCase):
   '''
   Test class that defines the test cases for the User class behavior.
   
   Args:
-      unittest.Testcase: TestCase class that helps in creating cases.
+      unittest.TestCase: TestCase class that helps in creating cases.
   '''
   
   def setUp(self):
     '''
-    Method to create a new instance of the User class before each test case to be declared.
+    Method to create a new instance of the User class before each test is run.
     '''
     
     self.new_user = User('Stl','pswd1234')
     
-    # ======== FIRST TEST - CORRECT INSTANTIATION ====
+    # ======== FIRST (User Class) TEST - CORRECT INSTANTIATION ====
   def test__init__(self):
     '''
     Test if the creation of new User instances is properly done.
@@ -25,7 +26,7 @@ class TestUser(unittest.TestCase):
     self.assertEqual(self.new_user.password,'pswd1234')
     
     
-    # ======= SECOND TEST - SAVING NEW USER =======
+    # ======= SECOND (User Class) TEST - SAVING NEW USER =======
   def test_save_new_user(self):
     '''
     Test if the user object is saved into the users list
@@ -33,6 +34,38 @@ class TestUser(unittest.TestCase):
     self.new_user.save_new_user() # saving the new user
     self.assertEqual(len(User.users),1)
     
+
+class TestCredentials(unittest.TestCase):
+  '''
+  Test class that defines the test cases for the Credentials class behavior.
+  
+  Args:
+    unittest.TestCase: TestCase class that helps in creating cases.
+  '''
+
+  def setUp(self):
+    '''
+    Method to create a new instance of the Credentials class before each test is run.
+    '''
+    self.new_credential = Credentials('twitter', 'SuperG', 'pwd333')
+    
+    # ======== FIRST (Credentials Class) TEST - CORRECT INSTANTIATION ====
+  def test__init__(self):
+    '''
+    Test if the creation of new Credentials instances is properly done.
+    '''
+    self.assertEqual(self.new_credential.account, 'twitter')
+    self.assertEqual(self.new_credential.acc_username, 'SuperG')
+    self.assertEqual(self.new_credential.acc_password, 'pwd333')
+
+
+
+
+
+
+
+
+
     
 if __name__ == '__main__':
   unittest.main()
