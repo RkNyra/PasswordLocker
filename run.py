98c00e4e0bab2a65789623bb1d3cc3e895cef229
RKNyra <rk.nyra@gmail.com>
2019-10-06 23:01:07 +0300

    create a function (in run.py) that saves a new password locker user account

--- run.py
+++ run.py
@@ -2,9 +2,18 @@
 from user import User
 from credentials import Credentials
 
+#==== create user account ======
 def create_pswdlckr_account(username,password):
   '''
   Function to create a new password locker account
   '''
   new_user = User(username,password)
   return new_user
+
+#===== save the newly created account ====
+def save_account(credential):
+  '''
+  Functin to save new pswdlckr user account
+  '''
+  credential.save_account()
+
